f9080fb373f6d0c74c983caebd80d6ffe03cc7ea
Abdo-A <abdopoetry@gmail.com>
2018-04-07 19:56:51 +0200

    Made the snake automatically moving

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const snake = document.getElementById('snake');
+const apple = document.getElementById('apple');
 const input = document.getElementById('helperInput');
+const eye = document.getElementById('eye-up');
+const eye_up = document.getElementById('eye-up');
+const eye_down = document.getElementById('eye-down');
 let speed=20;
+let key, lastKey;
 
 setInterval(()=>{
     input.focus();
-},50);
+},100);
+
+getApple();
+let i=0;
+function startMove(event){
+    key = event? event.which || event.keyCode:lastKey;
+    if(i==0)
+    setInterval(()=>{
+        move(event);
+    },100);
+    i=1;
+}
 
 function move(event){
-    var key = event.which || event.keyCode; 
+     
     //document.getElementById("test").innerHTML = "The Unicode KEY code is: " + key;
     if(key==37){ //left
         snake.style.left=parseInt(snake.style.left)-speed+'px';
@@ -40,10 +56,12 @@ function move(event){
     if(key==81){ //Q
         speed+=5;
         document.getElementById("test").innerHTML = "High!";
+        startMove();
     }
     if(key==65){ //A
         speed-=5;
         document.getElementById("test").innerHTML = "Low!";
+        startMove();
     }
     if(speed<=0){
         speed=2;
@@ -51,5 +69,42 @@ function move(event){
     if(speed>=150){
         speed=150;
     }
-    console.log(key);
+    checkTouch();   
+    //console.log("Snake",snakeDim.top, snakeDim.right, snakeDim.bottom, snakeDim.left);
+    //console.log("Apple",appleDim.top, appleDim.right, appleDim.bottom, appleDim.left);
+    lastKey=key;
+}
+
+
+function getApple(){
+    apple.style.left=Math.floor(Math.random() * (screen.width-200 - 0 + 1)) + 0 +'px';
+    apple.style.top=Math.floor(Math.random() * (screen.height-200 - 0 + 1)) + 0 +'px';
+}
+
+function checkTouch(){
+    let appleDim = apple.getBoundingClientRect(); //appleDim.top, appleDim.right, appleDim.bottom, appleDim.left
+    let eyeDim= eye.getBoundingClientRect();
+
+    if((eyeDim.left<appleDim.left+50&&eyeDim.left>appleDim.left-50)
+        &&(eyeDim.top<appleDim.top+50&&eyeDim.top>appleDim.top-50)
+        ) {
+            getApple();
+            snake.style.width=parseInt(snake.style.width)+10+'px';
+            console.log(snake.style.width);
+            setTimeout(()=>{
+                eye_up.classList.remove('eye-normal');
+                eye_up.classList.add('eye-big');
+                eye_down.classList.remove('eye-normal');
+                eye_down.classList.add('eye-big');
+            },200);
+            setTimeout(()=>{
+                eye_up.classList.add('eye-normal');
+                eye_up.classList.remove('eye-big');
+                eye_down.classList.add('eye-normal');
+                eye_down.classList.remove('eye-big');
+            },2000);
+    }
+    // document.getElementById("test").innerHTML =`Apple left:${appleDim.left}, Apple top:${appleDim.top}`
+    // console.log('Eye left:', eyeDim.left);
+    // console.log('Eye top:', eyeDim.top);
 }
